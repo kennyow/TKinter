e1881aaf70b9252b71e8735fc78e8e77ac9ad26e
kennyow <kennyow86@gmail.com>
2024-03-14 15:25:45 -0300

    add sticky, row, column, grid

--- app.py
+++ app.py
@@ -7,29 +7,35 @@ def greet():
     print(f"IAEEEW, {user_name.get() or 'World'}!")
 
 root = tk.Tk() #Creating a Tk object
+root.title("MARSUPILAMI")
+
+root.columnconfigure(0, weight = 1)
 
 user_name = tk.StringVar() #Cria um placeholder
 
 input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
-input_frame.pack(fill='both')
+input_frame.grid(row=0, column = 0, sticky="EW")
 
 name_label = ttk.Label(input_frame,text="Nome: " )
-name_label.pack(side="left", padx=(0,10))
+name_label.grid(row=0, column = 0, padx=(0,10))
 
 name_entry =ttk.Entry(input_frame,width = 15, textvariable= user_name) #width é a quantidade de caracteres
-name_entry.pack(side="left")
+name_entry.grid(row=0, column = 1)
 name_entry.focus()
 
 buttons = ttk.Frame(root, padding=(20,10))
-buttons.pack(fill='both')
+buttons.grid(row=1, column=0, sticky="EW") #East and West ficam presos
+
+buttons.columnconfigure(0, weight = 1)
+buttons.columnconfigure(1, weight = 1)
 
 greet_button = ttk.Button(buttons, text="Aperta!", command=greet) #insere a aplicação princial root, command é a função que o botão exerce
 #greet_button.pack() #Coloca o botão na janela ficando centralizado
 #* Da forma que está, abre a janela com o botão, e o print aparece no terminal
-greet_button.pack(side='left', fill='both', expand=True) #Coloca o botão na janela ficando à esquerda, fill preenche o espaço em branco com o botão por inteiro no eixo x, y ou os dois (both)(visível ao extender a janela utilizando expand)
+greet_button.grid(row=0, column = 0, sticky="EW") #Coloca o botão na janela ficando à esquerda, fill preenche o espaço em branco com o botão por inteiro no eixo x, y ou os dois (both)(visível ao extender a janela utilizando expand)
 
 
 #Acrescentando o botão quit
 quit_button = ttk.Button(buttons, text="Sair", command=root.destroy) #destroy destrói o root
-quit_button.pack(side='left', fill='both', expand=True)#Coloca o botão  Sair na janela, à esquerda
+quit_button.grid(row=0, column = 1, sticky="EW")#Coloca o botão  Sair na janela, à esquerda
 root.mainloop()
