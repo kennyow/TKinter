99523ddd7a14c0f7b6a251e182a48aaff1f7afa9
kennyow <kennyow86@gmail.com>
2024-03-22 16:53:46 -0300

    dc1.py finalized

--- Distance Converter/dc1.py
+++ Distance Converter/dc1.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 from windows import set_dpi_awareness
+import tkinter.font as font
 
 set_dpi_awareness()
 
@@ -11,24 +12,57 @@ root = tk.Tk()
 #Title
 root.title("Distance Converter")
 
+font.nametofont("TkDefaultFont").configure(size=15)
+
+#Values
+metres_value = tk.StringVar()
+feet_value = tk.StringVar(value="Feet shown here")
+
+
+#Function to calculate feet:
+def calculate_feet(*args):
+    try:
+        metres = float(metres_value.get())
+        feet = metres * 3.28084
+        feet_value.set(f"{feet:.3f} ")
+
+    except ValueError:
+        pass
+
+
 root.columnconfigure(0, weight=1)
 main = ttk.Frame(root, padding=(30, 15))
 main.grid()
 
+#Name Metres
 metres_label = ttk.Label(main, text = "Metres: ")
-metres_input = ttk.Entry(main, width = 10)
-feet_label = ttk.Label(main, text="Feet:")
-feet_display = ttk.Label(main, text="Feet shown here")
-calc_button = ttk.Button(main, text="Calculate")
+metres_label.grid(column=0, row=0, sticky="w")
 
-metres_label.grid(column=0, row=0, sticky="w", padx=5, pady = 5)
-metres_input.grid(column=1, row=0, sticky="ew", padx=5, pady = 5)
+#Input
+metres_input = ttk.Entry(main, width = 10, textvariable=metres_value, font=("Segoe UI", 15))
+metres_input.grid(column=1, row=0, sticky="ew")
 metres_input.focus()
 
-feet_label.grid(column=0, row=1, sticky="w", padx=5, pady = 5)
-feet_display.grid(column=1, row = 1, sticky="ew", padx=5, pady = 5)
+#Name Feet
+feet_label = ttk.Label(main, text="Feet:")
+feet_label.grid(column=0, row=1, sticky="w")
 
-calc_button.grid(column=0, row=2, columnspan=2, sticky="ew", padx=5, pady = 5)
+#Display
+feet_display = ttk.Label(main, textvariable=feet_value)
+feet_display.grid(column=1, row = 1, sticky="ew")
 
+
+#Calculate
+calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
+calc_button.grid(column=0, row=2, columnspan=2, sticky="ew")
+
+#Put pads and pady on all grids
+for child in main.winfo_children():
+    child.grid_configure(padx=15, pady=15)
+
+
+
+root.bind("<Return>", calculate_feet)
+root.bind("<KP_Enter>", calculate_feet)
 root.mainloop()
 
