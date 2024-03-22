e1b7d9ab4dc84625f57eb97f8d37319af5777204
kennyow <kennyow86@gmail.com>
2024-03-22 17:21:41 -0300

    change name to poo

---
+++ Classe/poo.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from windows import set_dpi_awareness
+import tkinter.font as font
+
+set_dpi_awareness()
+
+
+
+class UserInputFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.user_input = tk.StringVar()
+
+        label = ttk.Label(self, text= "Enter your name: ")
+        entry = ttk.Entry(self, textvariable=self.user_input)
+        button = ttk.Button(self, command=self.greet, text="Rainha")
+
+        label.pack(side= 'left')
+        entry.pack(side= 'left')
+        button.pack(side= 'left')
+
+    def greet(self):
+        print(f"Hello, {self.user_input.get()}!")
+
+root = tk.Tk()
+
+frame = UserInputFrame(root)
+frame.pack()
+
+root.mainloop()
+
